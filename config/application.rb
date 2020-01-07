f400200acb3f63461dcb739d7940490df267aec9
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2020-01-06 21:08:48 -0800

    Update config

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module WorkLog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.action_view.form_with_generates_remote_forms = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
