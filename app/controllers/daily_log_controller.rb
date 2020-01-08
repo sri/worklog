1490de091951a43a4f109fb48daab6f0d741e1e7
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2020-01-07 20:02:49 -0800

    Group by day

--- app/controllers/daily_log_controller.rb
+++ app/controllers/daily_log_controller.rb
@@ -1,6 +1,11 @@
 class DailyLogController < ApplicationController
   def index
-    @daily_logs = DailyLog.all.order(created_at: :desc)
+    @daily_logs = {}
+    DailyLog.all.order(created_at: :desc).each do |daily_log|
+      timestamp = daily_log.created_at.strftime("%Y-%m-%d")
+      @daily_logs[timestamp] ||= []
+      @daily_logs[timestamp] << daily_log
+    end
   end
 
   def create
